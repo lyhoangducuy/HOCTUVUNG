ecb1bbe114e495a864d2e8e092aa89e72fabee7a
lyhoangducuy <lyhoangducuy@gmail.com>
2025-08-03 14:57:08 +0700

    sua setup trang chu layout

--- hoctuvung-frontend/src/layouts/TrangChuLayout/TrangChuLayout.jsx
+++ hoctuvung-frontend/src/layouts/TrangChuLayout/TrangChuLayout.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-
+import { Outlet } from 'react-router-dom';
 export default function TrangChuLayout() {
   return (
     <div>
-        <TrangChuHeader/>
         <Outlet />
-        <TrangChuFooter />
     </div>
   )
 }
