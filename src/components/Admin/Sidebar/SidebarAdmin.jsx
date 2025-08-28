adc09412219246a70f97e353aecbcfcc1958d078
lyhoangducuy <lyhoangducuy@gmail.com>
2025-08-28 15:01:30 +0700

    fix side bar lap

--- src/components/Admin/Sidebar/SidebarAdmin.jsx
+++ src/components/Admin/Sidebar/SidebarAdmin.jsx
@@ -67,56 +67,6 @@ const SidebarAdmin = () => {
             </li>
           </ul>
         </nav>
-        <ul>
-          <li>
-            <a href="/admin/thong-ke">
-              <span className="sidebar-icon">
-                <FaChartBar />
-              </span>
-              Thống Kê
-            </a>
-          </li>
-          <li>
-            <a href="/admin/quan-ly-user">
-              <span className="sidebar-icon">
-                <FaUser />
-              </span>
-              Quản Lý Người Dùng
-            </a>
-          </li>
-          <li>
-            <a href="/admin/quan-ly-bo-the">
-              <span className="sidebar-icon">
-                <FaLayerGroup />
-              </span>
-              Quản Lý Bộ Thẻ
-            </a>
-          </li>
-          <li>
-            <a href="/admin/quan-ly-khoa-hoc">
-              <span className="sidebar-icon">
-                <FaChalkboardTeacher />
-              </span>
-              Quản Lý Khóa Học
-            </a>
-          </li>
-          <li>
-            <a href="/admin/quan-ly-video">
-              <span className="sidebar-icon">
-                <FaVideo />
-              </span>
-              Quản Lý Video
-            </a>
-          </li>
-          <li>
-            <a href="/admin/quan-ly-tra-phi">
-              <span className="sidebar-icon">
-                <FaMoneyCheckAlt />
-              </span>
-              Quản Lý Trả Phí
-            </a>
-          </li>
-        </ul>
       </div>
     </aside>
   );
